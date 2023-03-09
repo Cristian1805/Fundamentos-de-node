aa992c2ae44e548c537696090a493c8dbe4cd87f
Cristian1805 <cristiancamilolozanogomez@gmail.com>
2023-03-09 15:30:20 -0500

    first commit

---
+++ flecha.js
@@ -0,0 +1,13 @@
+const suma = (a,b) => {
+    return (a + b)
+} 
+
+
+
+
+const saludar = () =>{
+    return ("hola mundo")
+}
+
+console.log(suma(6,10))
+console.log(saludar())
---
+++ template-string.js
@@ -0,0 +1,11 @@
+const nombre = 'deadpool';
+
+const real = 'Wide Winston';
+
+const normal = nombre + ' ' + real;  
+const template = `${ nombre } ${real}`;
+
+console.log(normal);
+console.log(template)
+
+console.log(normal === template);
